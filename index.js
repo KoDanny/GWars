6444f5f69d7e3817a448f5fb68d64470e50db852
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-05-14 21:57:52 +0300

    webpack 4

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client';
 import { FirstComponent } from './src/components/firstComponent';
 
 const root = ReactDOM.createRoot(document.querySelector('#my_main_div'));
-
+console.log(root);
 root.render(<FirstComponent />);
--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,5 +1,6 @@
 import { f1 } from '../module1';
 
 export const FirstComponent = () => {
+	console.log();
 	return <div>РЕАКТ</div>;
 };
