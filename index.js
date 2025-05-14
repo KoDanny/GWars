078dcc9f51504599be51c0715cf63b4f2437a570
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-05-14 22:38:38 +0300

    webpack 8

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { FirstComponent } from './src/components/firstComponent';
+const { f1 } = require('./src/module1');
 
-const domNode = document.getElementById('my_main_div');
-const root = createRoot(domNode);
-
-root.render(<FirstComponent />);
+f1();
--- src/components/FirstComponent.js
+++
@@ -1,6 +0,0 @@
-import { f1 } from '../module1';
-
-export const FirstComponent = () => {
-	console.log('FIRST COMPONENT');
-	return <div className='App'>РЕАКТ</div>;
-};
