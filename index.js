c2c22020b3daf159ade1e135962863830d1b2356
Komogorov Denis <komogorov.denis1990@bk.ru>
2025-05-14 19:06:11 +0300

    6

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// ==UserScript==
+// @name         Git Test
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-14
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.gwars.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gwars.io
+// @require      https://raw.githubusercontent.com/KoDanny/GWars/refs/heads/main/index.js
+// @grant        none
+// ==/UserScript==
+
 import { foo2 } from './src/module2.js';
 
+foo2();
 export const script = { foo2 };
